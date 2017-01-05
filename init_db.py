2df29070fbc5d949a2c8407033c5ec7f3bc37a0b
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-01-05 22:25:06 +0100

    Add init_db.py

--- init_db.py
+++ init_db.py
@@ -20,4 +20,4 @@ for c in confs:
 for t in tables:
     db.events.insert_one({'name': t, 'quota': 30, 'type': 'table_ronde', 'places_left': {'first': 30, 'second': 30}})
 
-#db.users.update_many({'$set'})
+db.users.update_many({}, {'$set': {'events.joi': {}}})
