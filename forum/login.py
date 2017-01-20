ffd95340fbb41bb2a19f68bca7e9ea400e708255
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-01-20 11:32:43 +0100

    Extend confirm link expiration

--- forum/login.py
+++ forum/login.py
@@ -29,7 +29,7 @@ def generate_confirmation_token(email):
     return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
 
 
-def confirm_token(token, expiration=3600):
+def confirm_token(token, expiration=7200):
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     try:
         email = serializer.loads(
