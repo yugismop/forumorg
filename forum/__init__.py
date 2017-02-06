6a2358fcd8b1a7b02f6f8237325825268a134568
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-02-06 14:40:32 +0100

    Quick fix v2

--- forum/__init__.py
+++ forum/__init__.py
@@ -33,7 +33,8 @@ init_storage()
 @app.template_filter('to_companies')
 def to_companies(day):
     cur = get_db().companies.find({}, {'id': 1, 'name': 1, 'pole': 1, 'ambassadors.{}'.format(day): 1, '_id': 0})
-    cur = [l for l in cur if l['id'] != 'admin' and l['id'] != 'forumorg' and l['pole'] != 'school']
+    cur = [l for l in cur if l['id'] != 'admin' or l['id'] != 'forumorg']
+    cur = [l for l in cur if l['pole'] != 'school']
     res = []
     for c in cur:
         is_filled = True if c.get('ambassadors') else False
