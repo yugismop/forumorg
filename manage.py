d10aa376740b021f1b4a390d8bdce3c4c22b1fcd
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-02-21 14:59:22 +0100

    Change icons

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask_script import Manager
 from forum import app
 from pymongo import MongoClient
 import os
+import csv
 from flask_assets import ManageAssets
 
 
@@ -11,6 +12,16 @@ db = client.get_default_database()
 
 manager = Manager(app)
 manager.add_command("assets", ManageAssets())
+
+
+@manager.command
+def complete_companies():
+    path = os.path.join(os.path.dirname(__file__), 'data/companies.csv')
+    reader = csv.reader(open(path, 'rb'))
+    for row in reader:
+        db.companies.update({'id': row[0]})
+    #companies = [row for row in reader][1:]
+    #print(companies[0])
 
 
 @manager.command
