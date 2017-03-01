a432080d55b6ad2dda6dc676fc03fc90cc5356ae
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-03-01 11:21:18 +0100

    Add fix dates

--- manage.py
+++ manage.py
@@ -15,6 +15,19 @@ manager.add_command("assets", ManageAssets())
 
 
 @manager.command
+def fix_dates_():
+    from datetime import datetime
+    cur = db.users.find({}, {'_id': 0, 'registered_on': 1, 'confirmed_on': 1})
+    for c in cur:
+        if type(c['registered_on']) == str:
+            print('fixing this: {}'.format(c['registered_on']))
+            #datetime.strptime(cur['registered_on'], '%a, %d %b %Y %H:%M:%S %Z')
+        if type(c['confirmed_on']) == str:
+            print('fixing this: {}'.format(c['confirmed_on']))
+            #datetime.strptime(cur['registered_on'], '%a, %d %b %Y %H:%M:%S %Z')
+
+
+@manager.command
 def drop_schools_():
     cur = db.users.find({'profile.school_': {'$exists': True}}, {'_id': 0, 'profile.school_': 1})
     for c in cur:
