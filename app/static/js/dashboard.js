3c840e210535b432c335c477052b9531bededc50
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-03-20 17:17:12 +0100

    Remove all js erros on users

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -6,9 +6,28 @@ $(function() {
     $("#datetime").inputmask("datetime", {
         "placeholder": "jj/mm/aaaa hh:ss"
     });
-    $("#phone").intlTelInput(opt("MOBILE"));
-    $("#phone_stand").intlTelInput(opt("MOBILE"));
-    $("#phone_facturation").intlTelInput(opt("FIXED_LINE"));
+    var options = {
+        autoPlaceholder: "aggressive",
+        dropdownContainer: "body",
+        geoIpLookup: function(callback) {
+            $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+                var countryCode = (resp && resp.country) ?
+                    resp.country :
+                    "";
+                callback(countryCode);
+            });
+        },
+        placeholderNumberType: "MOBILE",
+        initialCountry: "auto",
+        preferredCountries: [
+            'fr', 'ch', 'be', 'ma', 'nl'
+        ],
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.0/js/utils.js"
+    };
+    $("#phone").intlTelInput(options);
+    $("#phone_stand").intlTelInput(options);
+    options.placholderNumberType = "FIXED_LINE";
+    $("#phone_facturation").intlTelInput(options);
     $("#persons").inputmask("9", {
         "placeholder": "x"
     });
