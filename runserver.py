cb8bbb8cbad1d2ce9d9342c4e6617d1b5922bd57
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-03-23 09:10:49 +0100

    Improve runserver.py

--- runserver.py
+++ runserver.py
@@ -1,7 +1,6 @@
-
-
 from app import app
 import os
 
-port = int(os.environ.get('PORT', 3000))
-app.run(host='127.0.0.1', port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='127.0.0.1', port=port, debug=True)
